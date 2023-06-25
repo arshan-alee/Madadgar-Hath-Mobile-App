9441d781dc7b7bf6fec5f6e0abda259004ef03eb
arshan_alee <mirarshanali@gmail.com>
2023-06-25 16:42:09 +0500

    Splash added

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:madadgarhath/screens/loginpage.dart';
+import 'dart:async';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(milliseconds: 5000), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const LoginPage(),
+          ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 134, 201, 255),
+        body: Center(
+          child: Image.asset(
+            "images/logo.png",
+            width: 220,
+            height: 220,
+          ),
+        ),
+      ),
+    );
   }
 }
